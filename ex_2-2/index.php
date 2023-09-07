6e3ba901c1c9db879c149a6e035e352870af30f9
Huệ <137085692+Hue030324@users.noreply.github.com>
2023-09-07 22:26:48 +0700

    Add files via upload

---
+++ ex_2-2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Future Value Calculator</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+    <main>
+        <h1>Future Value Calculator</h1>
+        <form action="">
+            <div id="data">
+                <label for="">Investment Amount:</label>
+
+            </div>
+        </form>
+    </main>
+
+</body>
+
+</html>
